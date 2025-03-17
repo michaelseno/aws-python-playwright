6113024d7615595aa269428ab997fbd249053df2
Michael Jordan Seno <mjseno@Michaels-MacBook-Pro-2.local>
2025-03-17 22:59:12 +0800

    added screenshot, video recording feature and updated logging

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,9 +1,9 @@
-from playwright.sync_api import Page
 from pages.base_page import BasePage
 
+
 class HomePage:
-    def __init__(self, page: Page):
-        self.__page = BasePage(page)
+    def __init__(self, browser):
+        self.__page = BasePage(browser)
         self.__page_url = "http://aws-personal-portfolio-bucket.s3-website-us-east-1.amazonaws.com"
         self.__header_locator = "text=Welcome to My Cloud Portfolio"
         self.__about_locator = "#about"
@@ -20,5 +20,3 @@ class HomePage:
         self.__page.validate_locator(self.__skills_locator)
         self.__page.validate_locator(self.__projects_locator)
         self.__page.validate_locator(self.__contact_locator)
-
-
